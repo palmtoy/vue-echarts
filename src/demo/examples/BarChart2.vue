1ead29c5d706ffb9cf65ae8fd0d578409e48eb31
lizhigang <lizhigang@magictavern.com>
2024-12-02 15:08:05 +0800

    * mv func getData into BarChart2

--- src/demo/examples/BarChart2.vue
+++ src/demo/examples/BarChart2.vue
@@ -5,7 +5,6 @@ import { GridComponent, DatasetComponent } from "echarts/components";
 import { shallowRef, onBeforeUnmount } from "vue";
 import VChart from "../../ECharts";
 import VExample from "./Example";
-import getData from "../data/bar2";
 
 use([BarChart, DatasetComponent, GridComponent]);
 
@@ -40,6 +39,115 @@ function refresh() {
   loading.value = true;
 
   timer = setInterval(tick, 1000);
+}
+
+function randomData(multiplier) {
+  const maxNum = 7;
+  const data = [];
+  for (let i = 0; i < maxNum; i++) {
+    const rdm = Math.round((1 + Math.random()) * multiplier * 100);
+    data.push(rdm);
+  }
+  return data;
+}
+
+function getData() {
+  return {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        // Use axis to trigger tooltip
+        type: "shadow" // 'shadow' as default; can also be 'line' or 'shadow'
+      }
+    },
+    legend: {},
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true
+    },
+    // Horizontal bar chart so x-axis is value, y-axis is category
+    xAxis: {
+      type: "value"
+    },
+    yAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    },
+    /*
+    // Vertical bar chart so x-axis is category, y-axis is value
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    },
+    yAxis: {
+      type: "value"
+    },
+    */
+    series: [
+      {
+        name: "Direct",
+        type: "bar",
+        stack: "total",
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: "series"
+        },
+        data: randomData(3)
+      },
+      {
+        name: "Mail Ad",
+        type: "bar",
+        stack: "total",
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: "series"
+        },
+        data: randomData(1)
+      },
+      {
+        name: "Affiliate Ad",
+        type: "bar",
+        stack: "total",
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: "series"
+        },
+        data: randomData(2)
+      },
+      {
+        name: "Video Ad",
+        type: "bar",
+        stack: "total",
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: "series"
+        },
+        data: randomData(5)
+      },
+      {
+        name: "Search Engine",
+        type: "bar",
+        stack: "total",
+        label: {
+          show: true
+        },
+        emphasis: {
+          focus: "series"
+        },
+        data: randomData(8)
+      }
+    ]
+  };
 }
 </script>
 
